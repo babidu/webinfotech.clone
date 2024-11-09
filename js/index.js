c7d7ff08cd03cfbd8d3e43f260c1853e0cb0675f
Babidul Hassan <126752146+babidu@users.noreply.github.com>
2024-11-09 11:26:10 +0530

    new project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1, 
+  spaceBetween: 30, 
+  centeredSlides: true, 
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 3000, 
+//     disableOnInteraction: false, 
+//   },
+//   speed: 800, 
+});
